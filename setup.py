749cb61f6d55b2dedcfd28f962846bfd7cf4c0d2
Moritz Hartlieb <moritz-hartlieb@web.de>
2021-08-23 16:50:46 +0200

    pypi test

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setuptools.setup(
     name="IF_LICENSE_PLATES_COULD_TALK",
     version="0.0.1",
     author="Moritz Hartlieb",
-    author_email="moritz-hartlieb@web.de",
     description="German license plates' geographical information is used to link them to income and crime.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,10 +14,28 @@ setuptools.setup(
     project_urls={
         "Bug Tracker": "https://github.com/dullibri/if_license_plates_could_talk/issues",
     },
-    classifiers=[],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
+    install_requires=[
+        "bs4",
+        "dash",
+        "dash-bootstrap-components",
+        "dash-core-components",
+        "dash-html-components",
+        "geopandas",
+        "matplotlib",
+        "numpy",
+        "openpyxl",
+        "pandas",
+        "plotly",
+        "pyproj",
+        "requests",
+        "scikit-learn",
+        "scipy",
+        "statsmodels",
+        "xlrd==2.0.1"
+    ],
     package_data={
         "": ["data", "geo"]
     }
