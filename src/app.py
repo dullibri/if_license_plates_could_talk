816f36a2268395bcf8e129d595a06f3b47acd792
Moritz Hartlieb <moritz-hartlieb@web.de>
2021-08-23 15:15:59 +0200

    heroku deployment

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 import visualization.app
 import data
 
+app = visualization.app.VisApp()
+server = app.app.server
+
 if __name__ == '__main__':
-    app = visualization.app.VisApp()
-    server = app.app.server
     app.run()
