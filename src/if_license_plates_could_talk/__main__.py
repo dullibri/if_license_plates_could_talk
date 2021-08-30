189f763d78d8c0eb4a31c7d35ed5f3aedb088eaa
Moritz Hartlieb <moritz-hartlieb@web.de>
2021-08-30 10:29:17 +0200

    fix packaging

--- src/if_license_plates_could_talk/__main__.py
+++ src/if_license_plates_could_talk/__main__.py
@@ -3,13 +3,14 @@ from . import setup_db
 import argparse
 
 parser = argparse.ArgumentParser(description="IF_LICENSE_PLATES_COULD_TALK")
-parser.add_argument("cmd")
+parser.add_argument("cmd", nargs="?", default="run")
+
 args = parser.parse_args()
 
 if args.cmd == "prep":
     prep_data.prep_data()
 elif args.cmd == "db":
     setup_db.setup_db()
-elif args.cmd == "run":
+else:
     from . import app
     app.run()
